09d19a5e271cdff10310498ec0b2b31ac6dffef9
YatzuuValz <yahya1muhandar@gmail.com>
2025-08-21 22:41:26 +0700

    header

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <div className='bg-neutral-700 h-15 flex items-center'>
-        <p className='text-white px-10 font-bold uppercase'>ABC Restaurant</p>
+        <p className='text-white px-11 font-bold uppercase'>ABC Restaurant</p>
         {/* <img src="" alt="" /> */}
     </div>
   )
