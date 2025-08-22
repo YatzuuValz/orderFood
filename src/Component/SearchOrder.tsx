cc874c87acd0591ea8443e55f5d8864603380c40
YatzuuValz <yahya1muhandar@gmail.com>
2025-08-22 19:49:06 +0700

    search only when enter

--- src/Component/SearchOrder.tsx
+++ src/Component/SearchOrder.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 
 type Props={
     setPencarian:React.Dispatch<React.SetStateAction<string>>
@@ -6,23 +6,22 @@ type Props={
 function SearchOrder({setPencarian}:Props) {
     const [search,setSearch] = useState('')
     
-    // function handleOnKeyDown(event:KeyboardEvent){
-    //     console.log('runing')
-    //     if(event.key === 'Enter'){
-    //         setPencarian(search)
-    //     }
-    // }
-    // useEffect(()=>{
-    //     console.log('run')
-    //     document.addEventListener('keydown',handleOnKeyDown)
-    //     return document.removeEventListener('keydown',handleOnKeyDown)
-    // },[])
+    const handleOnKeyDown= useCallback((event:KeyboardEvent)=>{
+        console.log('runing')
+        if(event.key === 'Enter'){
+            setPencarian(search)
+        }
+    },[search,setPencarian])
+    useEffect(()=>{
+        document.addEventListener('keydown',handleOnKeyDown)
+        return ()=> document.removeEventListener('keydown',handleOnKeyDown)
+    },[handleOnKeyDown])
 
     return (
         <div className='flex bg-neutral-700 p-3 h-14'>
                 <input type="text" placeholder='Search' value={search} onChange={(event)=>setSearch(event.target.value)}
-                    className='rounded-lg border border-white border-solid bg-white px-3 w-8/10 sm:w-1/2  h-full' />
-                {/* <img src="" alt="" /> */}
+                    className='rounded-l-lg border border-white border-solid bg-white px-3 lg:w-4/10 w-1/2  h-full' />
+                <img src="search.png" alt="" className='bg-white p-2 rounded-r-lg cursor-pointer' onClick={()=>setPencarian(search)} />
         </div>
     )
 }
